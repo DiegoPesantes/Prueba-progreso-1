17da0f5d3af831a7e9b95f39d2e5869ac0da132b
Laboratorio <laboratorio@udla-ec.int>
2023-10-24 14:32:49 -0500

    Segundo commit creracion sentencia for

---
+++ Prueba.c
@@ -0,0 +1,28 @@
+//Diseñe un programa que reciba como dato un número entero y a partir de este genere un número de un dígito (entre 0 y 9) sumando los dígitos tantas veces como sea necesario.
+
+#include <stdio.h>
+int main(){
+
+    
+    //Creacion de variables
+
+    int num=0;
+    int res=0;
+    int i;
+
+    //Pedido de numero al usuario
+
+    printf("Ingrese un numero entero");
+    scanf("%d",&num);
+
+    for ( i = 0; i < 9; i++)
+    {
+        /* code */
+    }
+    
+
+return 0;
+
+}
+
+
